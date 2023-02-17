aa721a97a7ac4768f8292e53b3d36261e21c4f6b
abellagnech <92161217+abellagnech@users.noreply.github.com>
2023-02-17 01:42:20 +0100

    it's not working

--- elm/src/Main.elm
+++ elm/src/Main.elm
@@ -1,195 +1,156 @@
-module Main exposing (..)
-
-import Http exposing (..)
-import Html.Events exposing (onInput)
-import Html exposing (..)
-import Html.Attributes exposing (..)
-import Json.Decode exposing (..)
-import Random
+module Main exposing (main)
 import Browser
-import File exposing (..)
+import Html exposing (..)
+import Html.Attributes exposing (placeholder, value, style , type_)
+import Html.Events exposing (onClick, onInput)
+import Http
+import Json.Decode exposing (Decoder)
+import String
+import Url
+import Url.Builder as UrlBuilder
+import Html.Events exposing (onInput, onClick , onCheck)
+import Http
+import  Task
+import Maybe exposing (..)
 
-
--- MODEL
 
 type alias Model =
-    { word : String
-    , definitions : List String
+    { wordList : List String
+    , currentWord : String
+    , currentDefinition : String
+    , guess : String
+    , guessResult : Maybe Bool
     }
 
 
-initialModel : Model
-initialModel =
-    { word = ""
-    , definitions = []
-    }
+init : () -> ( Model, Cmd Msg )
+init _ =
+    let
+        wordList =
+            -- Load the word list from a file
+            -- In this example, we assume the words are separated by newlines
+            "ListOfWords.txt"
+                |> Http.post { url = "ListOfWords.txt", body = Http.emptyBody, expect = Http.expectJson FetchWordListReceived wordListDecoder }
+                |> Task.perform identity
+                |> String.lines
+                |> List.filter (String.trim >> (/=) "")
+    in
+    ( Model wordList "" "" "" Nothing, Cmd.none )
+
+wordListDecoder : Decoder (List String)
+wordListDecoder =
+    Json.Decode.list Json.Decode.string
 
 
--- UPDATE
 
 type Msg
-    = NewWord String
-    | WordLoaded (Result Http.Error String)
-    | DefinitionsLoaded (Result Http.Error (List String))
+    = FetchWordListReceived (Result Http.Error String)
+    | FetchWordListFailed Http.Error
+    | FetchDefinitionReceived (Result Http.Error String)
+    | FetchDefinitionFailed Http.Error
+    | UpdateGuess String
+    | CheckGuess
+    | NextWord
+    | RestartGame
 
 
-update : Msg -> Model -> (Model, Cmd Msg)
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        NewWord word ->
-            ( { model | word = word }, Http.post WordLoaded (wordRequest word) )
+        FetchWordListReceived (Ok wordList) ->
+            ( { model | wordList = wordList }, Cmd.none )
 
-        WordLoaded (Ok word) ->
-            ( model, Http.post DefinitionsLoaded (definitionsRequest word) )
-
-        WordLoaded (Err _) ->
+        FetchWordListReceived (Err _) ->
             ( model, Cmd.none )
 
-        DefinitionsLoaded (Ok definitions) ->
-            ( { model | definitions = definitions }, Cmd.none )
-
-        DefinitionsLoaded (Err _) ->
+        FetchWordListFailed _ ->
             ( model, Cmd.none )
 
+        FetchDefinitionReceived (Ok definition) ->
+            ( { model | currentDefinition = definition }, Cmd.none )
 
-wordRequest : String -> String  
-wordRequest word =
-    Http.get
-        { method = "GET"
-        , headers = []
-        , url = "http://localhost:8000/randomWord?word=" ++ word
-        , body = Http.emptyBody
-        , expect = Http.expectString
-        , timeout = Nothing
-        , withCredentials = False
-        }
+        FetchDefinitionReceived (Err _) ->
+            ( model, Cmd.none )
 
+        FetchDefinitionFailed _ ->
+            ( model, Cmd.none )
 
-definitionsRequest : String -> (List String)
-definitionsRequest word =
-    Http.get
-        { method = "GET"
-        , headers = []
-        , url = "https://api.dictionaryapi.dev/api/v2/entries/en/" ++ word
-        , body = Http.emptyBody
-        , expect = Http.expectJson decodeDefinitions
-        , timeout = Nothing
-        , withCredentials = False
-        }
+        UpdateGuess guess ->
+            ( { model | guess = guess }, Cmd.none )
 
+        CheckGuess ->
+            let
+                isCorrect =
+                    model.guess == model.currentWord
+            in
+            ( { model | guessResult = Just isCorrect }, Cmd.none )
 
-decodeDefinitions : Decoder (List String)
-decodeDefinitions =
-    at [ "0", "meanings" ]
-        (Json.Decode.list
-            (at [ "definitions", "definition" ]
-                string
+        NextWord ->
+            let
+                word =
+                    List.map2 model.wordList
+            in
+            if List.isEmpty model.wordList then
+                ( model, Cmd.none )
+            else
+                ( { model | currentWord = word, currentDefinition = "", guess = "", guessResult = Nothing }
+                , Http.get ("https://api.dictionaryapi.dev/api/v2/entries/en/" ++ word ++ "?key=YOUR_API_KEY_HERE")
+                    |> Http.post { url = "https://api.dictionaryapi.dev/api/v2/entries/en/" ++ word ++ "?key=YOUR_API_KEY_HERE", body = Http.emptyBody, expect = Http.expectJson FetchDefinitionReceived definitionDecoder }
+
             )
-        )
 
+        RestartGame ->
+            ( { model | currentWord = "", currentDefinition = "", guess = "", guessResult = Nothing }, NextWord )
+definitionDecoder : Decoder (Result Http.Error String)
+definitionDecoder =
+    Json.Decode.at [ "0", "meanings", "0", "definitions", "0", "definition" ] Json.Decode.string
+        |> Json.Decode.map Result.Ok
+        |> Json.Decode.oneOf
+            [ Json.Decode.at [ "error" ] (Json.Decode.map Http.BadUrl <| Json.Decode.succeed "")
+            , Json.Decode.at [ "title" ] (Json.Decode.map Http.Timeout <| Json.Decode.succeed "")
+            , Json.Decode.at [ "detail" ] (Json.Decode.map Http.NetworkError <| Json.Decode.succeed "")
+            ]
 
--- VIEW
 
 view : Model -> Html Msg
-view gameState =
+view model =
     div []
-        [ h1 [] [ text "GuessIt" ]
-        , div [] [ text "Guess the word defined below:" ]
-        , viewWord gameState.word
-        , viewDefinitions gameState.definitions
-        , input [ type_ "text", onInput NewWord ] []
+        [ h1 [] [ text "Guess It!" ]
+        , if List.isEmpty model.wordList then
+            p [] [ text "No words available." ]
+          else
+            div []
+                [ if model.currentDefinition == "" then
+                    button [ onClick NextWord ] [ text "Start game" ]
+                  else
+                    div []
+                        [ h2 [] [ text model.currentWord ]
+                        , p [] [ text model.currentDefinition ]
+                        , case model.guessResult of
+                            Just True ->
+                                p [ style "color" "green" ] [ text "Correct" ]
+                            Just False ->
+                                p [ style "color" "red" ] [ text "Incorrect" ]
+                            Nothing ->
+                                Html.Attributes.form [ onInput UpdateGuess, onCheck CheckGuess ]
+                                    [ input [ type_ "text", value model.guess ] []
+                                    , button [ type_ "submit" ] [ text "Guess" ]
+                                    ]
+                        ]
+                , button [ onClick RestartGame ] [ text "Restart game" ]
+                ]
         ]
 
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
 
-viewWord : String -> Html msg
-viewWord word =
-    div [] [ text word ]
-
-
-viewDefinitions : List String -> Html msg
-viewDefinitions definitions =
-    ul [] (List.map (\definition -> li [] [ text definition ]) definitions)
-
-
--- API
-
-type alias ApiResponse =
-    { word : String
-    , definitions : List String
-    }
-
-
-decodeApiResponse : Decoder ApiResponse
-decodeApiResponse =
-    map2 ApiResponse
-        (field "word" string)
-        (field "definitions" (Json.Decode.list string))
-
-
-getDefinitionUrl : String -> String
-getDefinitionUrl word =
-    "https://api.dictionaryapi.dev/api/v2/entries/en/" ++ word
-
-
-getDefinitionCmd : String -> Cmd Msg
-getDefinitionCmd word =
-    Http.get
-        { url = getDefinitionUrl word
-        , expect = Http.expectJson (WordLoaded << List.head << .definitions) decodeApiResponse
-        }
-
-
--- MAIN
 
 main : Program () Model Msg
 main =
     Browser.element
-        { init = (initialModel, getWordFromFileCmd) -- Remplacer getWordCmd par getWordFromFileCmd
+        { init = init
         , update = update
         , view = view
-        , subscriptions = \_ -> Sub.none
+        , subscriptions = subscriptions
         }
-
--- GET RANDOM WORD
-
-getWordCmd : Cmd Msg
-getWordCmd =
-    let
-        wordGenerator =
-            Random.list 1 (Random.map String.words (File.read "ListOfWords.txt"))
-                |> Random.generate WordLoaded
-    in
-    Random.generate wordGenerator
-
-
--- HELPER FUNCTIONS
-
-splitByLine : String -> List String
-splitByLine input =
-    String.lines input
-        |> List.filter (\line -> String.trim line /= "")
-
-
-readFileCmd : String -> Cmd Msg
-readFileCmd fileName =
-    Http.get
-        { url = fileName
-        , expect = Http.expectString (WordLoaded << List.head << splitByLine)
-        }
-
-
--- INTEGRATE FILE READING
-
-getWordFromFileCmd : Cmd Msg
-getWordFromFileCmd =
-    readFileCmd "ListOfWords.txt"
-        |> Cmd.map List.head
-        |> Cmd.map (Maybe.withDefault "" >> String.trim)
-        |> Cmd.map NewWord
-
-
-getWordAndDefinitionCmd : String -> Cmd Msg
-getWordAndDefinitionCmd word =
-    Cmd.batch
-        [ NewWord word |> Http.post
-        , getDefinitionCmd word ]
